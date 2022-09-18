989289a6c1ce892b750dc9c0d675dbc7b9e848d3
Alfer <lohturoviv@yandex.ru>
2022-09-18 14:43:15 +0300

    Added solution fo the tusk

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_buy_button_exists(browser):
     browser.get(link)
+    time.sleep(10)
     btn = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert len(btn) != 0, "No buy button."
-    time.sleep(5)
